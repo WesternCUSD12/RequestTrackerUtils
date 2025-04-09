5ecd6b4e85dc528ebf5f7062c204533091831703
Justin Martin <jus10mar10@gmail.com>
2025-04-09 11:54:08 -0500

    Add additional config options.

--- app/config.py
+++ app/config.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 RT_TOKEN = os.getenv("RT_TOKEN", "default-token-if-not-set")
+WORKING_DIR = os.getenv("WORKING_DIR", "/var/lib/request-tracker-utils")  # Default working directory
 
-RT_URL = "https://tickets.wc-12.com"
+RT_URL = os.getenv("RT_URL", "https://tickets.wc-12.com")  # Default RT URL
 API_ENDPOINT = "/REST/2.0"
-LABEL_WIDTH_MM = 100
-LABEL_HEIGHT_MM = 62
-PREFIX = 'W12-'
-PADDING = 4
+LABEL_WIDTH_MM = int(os.getenv("LABEL_WIDTH_MM", 100))  # Default label width
+LABEL_HEIGHT_MM = int(os.getenv("LABEL_HEIGHT_MM", 62))  # Default label height
+PREFIX = os.getenv("PREFIX", "W12-")  # Default prefix for asset tags
+PADDING = int(os.getenv("PADDING", 4))  # Default padding for labels
